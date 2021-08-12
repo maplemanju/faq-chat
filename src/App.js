0c0da0fdf8b64ff04ef9f8640002311f10c63eff
clemente <elicran.on@gmail.com>
2021-08-12 20:32:48 +0900

    move main components to app file

---
+++ src/App.js
@@ -0,0 +1,192 @@
+import React from 'react';
+import './index.css';
+import BotStrings from "./BotStrings";
+import { SwitchTransition, CSSTransition } from 'react-transition-group';
+import { BrowserRouter } from "react-router-dom";
+import qAndA from './chatstrings.json'
+
+function CustomStrings(props) {
+  return (
+    <BrowserRouter>
+      <BotStrings needed={props.needed}/>
+    </BrowserRouter>
+  )
+}
+
+function Top(props) {
+  console.log(<CustomStrings needed="avatar" />);
+  return (
+    <div className="top">
+        <div className="avatar">
+          <CustomStrings needed="avatar" />
+        </div>
+        <span><CustomStrings needed="headerTxt"/></span>
+    </div>
+  );
+}
+
+function ChatArea(props) {
+
+  let scrollToBottom = (element, behavior) => {
+    element.scrollIntoView({behavior: behavior, block: "end"})
+  }
+
+  const loader = <span className="loader"></span>;
+  const {loading, chatType, chats, newRef} = props;
+  
+  return (
+    <div className="chat_area">
+    {chats.map((item,i, chat_ar) => {
+      const chatter = chatType[i];
+      const isLoading = loading && chat_ar.length - 1 === i;
+      const chatClass = 'chatter ' + chatter;
+      const avatar = chatter === 'support' ? <div className="avatar"><CustomStrings needed="avatar" /></div>: '';
+
+      return (
+        <div className={chatClass} key={i} ref={chat_ar.length - 1 === i? newRef : null}>
+          {avatar}
+          <div className="balloon">
+            <SwitchTransition>
+            <CSSTransition
+              key={isLoading}
+              classNames="fade"
+              onExit={()=>scrollToBottom(newRef.current, "smooth")}
+              onEnter={()=>scrollToBottom(newRef.current, "auto")}
+              addEndListener={(node, done) => {
+              node.addEventListener("transitionend", done, false);
+            }}>
+            <div>
+              { isLoading ? loader : item }
+            </div>
+            </CSSTransition>
+            </SwitchTransition>
+          </div>
+        </div>
+      );
+    }).reverse()}
+    </div>
+  ); //return
+}
+
+export default class ChatBot extends React.Component {
+  constructor(props) {
+    super(props);
+    this.newMsgref = React.createRef();
+    this.state = {
+      selection: qAndA.amayadori, //current index on json
+      chats: [qAndA.amayadori.bot], // chat array
+      chatType: ["support"], // support or customer
+      loading: false, // ellipsis loading
+      disableClick: false,
+      endOfChat: false
+    };
+  }
+
+  componentDidMount() {
+    this.scrollToBottom(this.newMsgref.current, "auto");
+    this.state.chats.push(this.choiceList(this.state.selection.choices));
+    this.state.chatType.push('customer');
+    setTimeout(() => {
+      this.setState({
+        chats: this.state.chats,
+        chatType: this.state.chatType,
+      });
+    this.scrollToBottom(this.newMsgref.current, "smooth");
+    }, 600);
+
+  }
+
+  scrollToBottom(element, behavior) {
+    element.scrollIntoView({behavior: behavior, block: "end"});
+  }  
+
+  choiceList(choices, chosen = -1) {
+    return (
+    <div className="choices">
+    { choices.map((choice,i) => {
+        const chosenClass = i===chosen ? 'chosen' : '';
+        return <button key={i} className={chosenClass} disabled={chosen>-1} onClick={() => this.handleClick(i)}>{choice}</button>
+    }) }
+    </div>
+    )
+  }
+
+  disablePastChoices(choice, choices) {
+    let { chats } = this.state;
+    chats[chats.length - 1] = this.choiceList(choices, choice);
+    this.setState({
+      chats: chats
+    });
+  }
+
+  handleClick(i) {
+    let { chats, selection, chatType } = this.state;
+
+    this.setState({
+      disableClick: true,
+      endOfChat: false
+    });
+
+    if(i<0) {
+      selection = qAndA.default;
+    } else {
+      this.disablePastChoices(i, selection.choices);
+      selection = selection.sub[i];
+    }
+
+    //push bot chat
+    chats.push(selection.bot);
+    chatType.push('support');
+    this.setState({loading: true});
+    setTimeout(() => {
+      this.setState({
+        selection: selection,
+        chats: chats,
+        loading: false,
+        chatType: chatType
+      });
+    }, 300);
+
+    //push choices chat
+    if (selection.choices) {
+      setTimeout(() => {
+        chatType.push('customer');
+        chats.push(this.choiceList(selection.choices));
+        this.setState({
+          chats: chats,
+          chatType: chatType,
+          disableClick: false
+        });
+        this.scrollToBottom(this.newMsgref.current, "smooth");
+      }, 2000);
+    } else {
+      this.setState({
+        disableClick: false,
+        endOfChat: true
+      });
+    }
+  }
+  
+  repeatButton() {
+    const btnClass = this.state.endOfChat ? "repeat_btn ask_repeat" : "repeat_btn";
+    return (
+      <button className={btnClass} disabled={this.state.disableClick} onClick={() => this.handleClick(-1)}>
+        <svg xmlns="http://www.w3.org/2000/svg" width="17" height="17" viewBox="0 0 24 24"><path d="M13.5 2c-5.629 0-10.212 4.436-10.475 10h-3.025l4.537 5.917 4.463-5.917h-2.975c.26-3.902 3.508-7 7.475-7 4.136 0 7.5 3.364 7.5 7.5s-3.364 7.5-7.5 7.5c-2.381 0-4.502-1.119-5.876-2.854l-1.847 2.449c1.919 2.088 4.664 3.405 7.723 3.405 5.798 0 10.5-4.702 10.5-10.5s-4.702-10.5-10.5-10.5z"/></svg>
+        repeat
+      </button>
+    )
+  }
+
+  render() {
+    const {chats, loading, chatType} = this.state;
+    return (
+      <div className="chatbody">
+      <Top/>
+      <ChatArea chats={chats} loading={loading} chatType={chatType} newRef={this.newMsgref}/>
+      <div className="bottom">
+        {this.repeatButton()}
+      </div>
+      </div>
+    );
+  }
+}
